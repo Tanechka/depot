af52e0a7acb4e95a6bba4174a82b52879cd94171
Tanechka <gvardi0@gmail.com>
2013-03-19 12:28:56 +0300

    pluralize

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -2,17 +2,12 @@ class StoreController < ApplicationController
   def index
     @products = Product.order(:title)
     @time = Time.now.strftime("%H:%M:%S") 
-
-    
-	@counter = session[:counter]
-
+  	@counter = session[:counter]
 	if @counter.nil?
 		@counter = 0;
 	else
 		@counter += 1;
 	end
-	session[:counter] = @counter
-		
-	
+	session[:counter] = @counter		
   end
 end

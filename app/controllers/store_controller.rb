c48ad641beedd95593b871454509c135131ed6a3
Tanechka <gvardi0@gmail.com>
2013-03-19 12:05:34 +0300

    session counter

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -2,5 +2,17 @@ class StoreController < ApplicationController
   def index
     @products = Product.order(:title)
     @time = Time.now.strftime("%H:%M:%S") 
+
+    
+	@counter = session[:counter]
+
+	if @counter.nil?
+		@counter = 0;
+	else
+		@counter += 1;
+	end
+	session[:counter] = @counter
+		
+	
   end
 end
